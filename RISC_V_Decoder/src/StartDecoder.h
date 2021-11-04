0c459e3ce7691fff52921b811fd75cc1455f7bba
Anu Patelkhana <apatelkh@uncc.edu>
2021-11-03 20:58:19 -0400

    RISC V Decoder Design

---
+++ RISC_V_Decoder/src/StartDecoder.h
@@ -0,0 +1,40 @@
+/***********************************************************************
+* FILENAME      :        StartDecoder.h
+*
+* DESCRIPTION   : contains class startdecoder which takes input , decodes it and prints out
+*                 the output.
+* AUTHOR        :   Anuradha Patelkhana
+*
+*
+************************************************************************/
+
+#pragma once
+#include "TypeDecoders.h"
+
+ 
+extern const char* codeDesc[]; 
+extern const char* signal[];
+class startdecoder  { 
+   
+    decoder * dd;
+    uint32_t inst;
+    uint16_t cnt;
+    uint32_t imm12, imm20;
+    int desc, rs1, rs2, rd;
+    char t;
+    void InsType();
+public:
+    startdecoder() : cnt(0), dd(NULL) {
+        inst =imm12 =imm20 = desc = rs1 = rs2 = rd = 0;
+        t = '\0';
+    }
+    startdecoder(uint32_t ins) : inst(ins), cnt(0) {
+      dd = new decoder(inst);
+      imm12 = imm20 = 0;
+        InsType();
+    }
+   
+    void printInstDescription();
+    void printControlSignals();
+
+   };
